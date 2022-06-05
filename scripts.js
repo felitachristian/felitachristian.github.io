747a602ea64bc7cb19f6fa4231d4b3ef1a7cf4b0
felitachristian <fchristian@student.unimelb.edu.au>
2022-06-05 18:19:50 +1000

    edits

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ function togglemenu(){
     document.getElementById('phonenavbar').classList.toggle('active');
  }
 
+function togglemenu() {
+    document.getElementById('addtocart').classList.toggle('active');
+}
